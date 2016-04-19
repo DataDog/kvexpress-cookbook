43e1257d21a73e0042edf3d9054c8cc724f5e627
Darron Froese <darron@froese.org>
2016-04-19 10:36:01 -0600

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'darron@froese.org'
 license 'Apache 2.0'
 description 'Installs/Configures kvexpress'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.1.0'
 recipe 'kvexpress::default', 'Installs/configures kvexpress'
 
 depends 'apt'
